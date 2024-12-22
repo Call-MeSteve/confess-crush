242e572c8de88f7fbb86e43ee7d986c02b6ace48
Call-MeSteve <quangminh.2004zz@gmail.com>
2024-12-22 21:09:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,24 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "FOR_MY_SWETIE",
+    introTitle: 'NGUYỄN PHẠM KHÁNH LINH',
+    introDesc: `Gửi tới empe iu của anh!!!
+    Các cụ có nói:
+    "Có yêu  thì nói rằng yêu
+    Chẳng yêu thì nói một điều cho xong.
+    Làm chi dở đục dở trong 
+    Lờ lờ nước hến cho lòng tương tư."
+      LÀM NGƯỜI YÊU ANH NHÉ LINH ƠI!!!`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Gửi cho empe <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'IU empe nhiều 🥰. Yêu yêu nhiều nhiều 😘😘',
+    messDesc: 'Noel tối nay, a qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: '    messLink: 'file:///E:/EMPEIU/-Merry-Christmas-to-my-crush-main/-Merry-Christmas-to-my-crush-main/kv1/timeline.html' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+
 }
